d33d5b4fecedef780a3ebe07d1c6672fe45a0a77
Ghania GN. Naveed <gn@target.com>
2023-09-27 09:30:03 +0100

    New file uploading project

---
+++ cypress/e2e/1-getting-started/File.cy.js
@@ -0,0 +1,16 @@
+
+describe('login into online communities using cypress', ()=>{
+    it('login successfully', () => {
+
+        cy.viewport(1920, 1080);
+         cy.visit('https://test-communities.oomi.co.uk/')
+        cy.get('#UserName').type('Tahir')
+         cy.get('#Password').type('mUp0J1/zu2yqFRQe4hVHMA==')
+         cy.get('.login-button').click()
+
+         //Navigate to announcemnet page
+         cy.get('#NavPage-67757 > a').click()
+
+         
+    })
+})
